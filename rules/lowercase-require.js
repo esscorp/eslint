30f679670672aaaf9f12dff47d9cdb6cd0f57a01
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-04-26 15:18:45 -0500

    Added rules

---
+++ rules/lowercase-require.js
@@ -0,0 +1,14 @@
+'use strict';
+//https://gist.github.com/jareware/7179093
+module.exports = function(context) {
+	return {
+		CallExpression: function(node){
+			var callee = node.callee;
+			var name = callee.name;
+			var param = (node.arguments.length)? node.arguments[0].value : false;
+			if (name === 'require' && typeof param === 'string')	{
+				if (param.toLowerCase() !== param) context.report(node, "Lowercase require() paths");
+			}
+		}
+	};
+};
