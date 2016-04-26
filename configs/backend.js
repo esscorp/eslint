6163353c573551ba1093fbd83c45597da3fe9bf3
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-04-26 12:24:53 -0500

    Updated backend.js

--- configs/backend.js
+++ configs/backend.js
@@ -5,9 +5,7 @@ module.exports = {
 		"space-infix-ops" : 0, //I like `(_.isArray(pkeys))? pkeys : [pkeys];`
 		"quotes": [0, "single", "avoid-escape"], // we like double outer quotes with single inner quotes for SQL and only single quotes for JS
 		"block-scoped-var": 2,
-		"keywords-spacing": [2, "always"],
 		"brace-style": [2, "1tbs"],
-		"space-in-brackets": [2, "never"],
 		"new-cap": [2, {"capIsNew": false}], //I like `Assert()
 		"no-underscore-dangle" : 0, //Using leading underscore to indicate private fnc
 		"camelcase": 0, //
@@ -33,7 +31,7 @@ module.exports = {
 		//formatting
 		"no-trailing-spaces": 2,
 		"eol-last": 2,
-		"semi": 2,
+		"semi": 2
 	},
 	"env": {
 		"node": true,
