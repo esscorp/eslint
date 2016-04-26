25225abe8e766df0240266480add1be98dc61977
Dan Gorby <GreenRaccoon23@gmail.com>
2016-04-26 12:08:38 -0500

    Update backend.js

--- configs/backend.js
+++ configs/backend.js
@@ -1,4 +1,5 @@
 module.exports = {
+	"extends": "eslint:recommended",
 	"rules": {
 		"curly": 0, //I like `if (error) return next(error);`
 		"space-infix-ops" : 0, //I like `(_.isArray(pkeys))? pkeys : [pkeys];`
@@ -16,9 +17,28 @@ module.exports = {
 
 		//our rules
 		"lowercase-require": 2 //deploy will fail otherwise
+
+		//this is node.js so allow console.* commands
+		"no-console": [0],
+
+		//spacing
+		"object-curly-spacing": [2, "never"],
+		"array-bracket-spacing": [2, "never"],
+		"computed-property-spacing": [2, "never"],
+		"keyword-spacing": [2, {"before": true, "after": true, "overrides": {}}],
+
+		//verify all node callbacks check for errors
+		"handle-callback-err": [2, "^(err|error|.+Error)$"],
+
+		//formatting
+		"no-trailing-spaces": 2,
+		"eol-last": 2,
+		"semi": 2,
 	},
 	"env": {
-		"node": true
+		"node": true,
+		"browser": false,
+		"mocha": true
 	},
 	"globals": {
 	}
