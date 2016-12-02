f149e3777b5ae7369a6df8fefc19d82f9f130d01
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-12-02 13:16:55 -0600

    Minor cleanup

--- configs/frontend.js
+++ configs/frontend.js
@@ -1,13 +1,16 @@
+'use strict';
+
 // eslint rules
 // 0 = off
 // 1 = generate warning
 // 2 = fail immediately
 
-//http://eslint.org/docs/rules/
+// http://eslint.org/docs/rules/
+
 module.exports = {
 	"rules": {
-		"curly": 0, //I like `if (error) return next(error);`
-		"space-infix-ops" : 0, //I like `(_.isArray(pkeys))? pkeys : [pkeys];`
+		"curly": 0, // allow `if (err) return next(err);`
+		"space-infix-ops" : 0, // allow no space before ternary operator, e.g., `(_.isArray(pkeys))? pkeys : [pkeys];`
 		"quotes": [2, "single", "avoid-escape"],
 		"block-scoped-var": 2,
 
@@ -17,11 +20,11 @@ module.exports = {
 		"computed-property-spacing": [2, "never"],
 		"keyword-spacing": [2, {"before": true, "after": true, "overrides": {}}],
 
-		"brace-style": [2, "1tbs"],
-		"new-cap": [2, {"capIsNew": false}], //I like `Assert()
-		"no-underscore-dangle" : 0, //Using leading underscore to indicate private fnc
-		"camelcase": 0, //
-		"no-shadow": 0, //we use the variable event everywhere
+		"brace-style": [2, "1tbs"], // require `if () {` and `} else {`
+		"new-cap": [2, {"capIsNew": false}], //`new Worker()`, not `new worker()`; allow `Assert()
+		"no-underscore-dangle" : 0, // allow leading underscore to indicate private functions
+		"camelcase": 0, // allow snake_case for SQL (use camelCase otherwise)
+		"no-shadow": 0, // allow variables to be overwritten in callbacks (e.g. `event`)
 		"strict": [1, "function"]
 	},
 	"env": {
