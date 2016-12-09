8592a9b71e240b30cac13513d500d2851ea9dafa
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-12-09 14:47:37 -0600

    Updated .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,4 @@
 'use strict';
 
-var Config = require('./configs/backend');
 
-delete Config.rules['lowercase-require'];
-
-
-module.exports = Config;
+module.exports = require('./configs/backend');
