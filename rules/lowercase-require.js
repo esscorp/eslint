b91e38b933f91a44b79816c3b53ef40ceed2f957
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2016-12-22 13:31:07 -0600

    Updated rule to 3.0.0 rule format

--- rules/lowercase-require.js
+++ rules/lowercase-require.js
@@ -1,15 +1,40 @@
 'use strict';
 
-//https://gist.github.com/jareware/7179093
-module.exports = function(context) {
-	return {
-		CallExpression: function(node){
-			var callee = node.callee;
-			var name = callee.name;
-			var param = (node.arguments.length)? node.arguments[0].value : false;
-			if (name === 'require' && typeof param === 'string')	{
-				if (param.toLowerCase() !== param) context.report(node, "Lowercase require() paths");
+
+// https://gist.github.com/jareware/7179093
+module.exports = {
+	meta: {
+		docs: {
+			description: 'disallow capital letters in `require()` statements',
+			category: 'Node.js and CommonJS',
+			recommended: true
+		},
+		fixable: false,
+		schema: []
+	},
+	create: function(context) {
+		return {
+			CallExpression: function(node) {
+
+				var callee = node.callee;
+				var name = callee.name;
+				var args = node.arguments;
+
+				if (name !== 'require') return;
+				if (args.length === 0) return;
+
+				var param = args[0].value;
+
+				if (typeof param !== 'string') return;
+
+				var valid = (param.toLowerCase() === param);
+				if (valid) return;
+
+				context.report({
+					message: 'Lowercase `require()` paths',
+					node: node
+				});
 			}
-		}
-	};
+		};
+	}
 };
