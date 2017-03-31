d11d32636a15fb676a3b27595f2e919229625c7c
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2017-03-31 13:52:02 -0500

    workaround for atom eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,3 +2,10 @@
 
 
 module.exports = require('./configs/backend');
+// atom eslint cannot load plugins unless the config file is in node_modules,
+// so remove our eslint-plugin-import's rules from config
+delete module.exports.extends[1];
+delete module.exports.rules['import/no-extraneous-dependencies'];
+delete module.exports.rules['import/no-unresolved'];
+delete module.exports.rules['lowercase-require'];
+delete module.exports.rules['async-series'];
