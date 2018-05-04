9521b362c9ab141bb528881626092ffcf566fef1
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2018-05-04 13:29:42 -0500

    callback-return

--- configs/backend.js
+++ configs/backend.js
@@ -291,8 +291,8 @@ module.exports = {
 		* Node.js and CommonJS
 		*****************************/
 
-		// // Always `return` from function when calling `next()`. todo: revisit
-		// "callback-return": [2, ["callback", "cb", "next"]],
+		// Always `return` from function when calling `next()`.
+		"callback-return": [2, ["callback", "cb", "next"]],
 
 		// All `require()` statements must always be in the top-level scope. Put them at the top of the file.
 		"global-require": 2,
